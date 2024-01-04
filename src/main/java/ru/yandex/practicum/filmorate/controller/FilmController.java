54af9bdb4a5794d2c6b4cdc0a3c9ff7b8d679efd
Scoobe <semenskakodub@yandex.ru>
2024-01-04 16:45:56 +0300

    9ый проект(2.1 правка)

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -38,14 +38,14 @@ public class FilmController {
     }
 
     @PutMapping
-    public Film put(@Valid @RequestBody Film film) {
+    public Film putFilm(@Valid @RequestBody Film film) {
         if (films.containsKey(film.getId())) {
             films.put(film.getId(), film);
             log.info("Не удалось поместить фильм");
             return film;
         } else {
-            log.info("Не удалось поместить фильм");
-            throw new InvalidIdException("Фильм с ID: " + film.getId() + " не обновлён", HttpStatus.NOT_FOUND);
+            log.error("Не удалось поместить фильм");
+            throw new InvalidIdException(String.format("Фильм с ID:`%d` не обновлён", film.getId()), HttpStatus.NOT_FOUND);
         }
     }
 }
